b8b869833c096d87b36145765f11644984e2be5c
Abdoulkarim <17a.karim@gmail.com>
2026-03-23 19:26:00 +0000

    feat: add global styling, metadata, and updates dependencies

---
+++ app/account/page.tsx
@@ -0,0 +1,9 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Account",
+};
+
+export default function page() {
+  return <div>Account</div>;
+}
--- app/cabins/page.tsx
+++ app/cabins/page.tsx
@@ -1,3 +1,11 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Cabins",
+  description:
+    "Browse our collection of cabins at The Oasis. Find your perfect retreat and book your stay today.",
+};
+
 export default function page() {
   return (
     <div>
