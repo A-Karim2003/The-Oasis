5396b7035091a314a98e3370d8dac96f7626ffbc
Abdoulkarim <17a.karim@gmail.com>
2026-03-23 08:22:47 +0000

    WIP: syncing work across PCs

--- app/cabins/[...slug]/page.tsx
+++ app/cabins/[...slug]/page.tsx
@@ -1,18 +1,22 @@
-import Link from "next/link";
-import { use } from "react";
-
-export default function Page({
+export default async function Page({
   params,
 }: {
   params: Promise<{ slug: string }>;
 }) {
-  const slug = use(params);
+  const { slug } = await params;
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await res.json();
   console.log(slug);
+  console.log(users);
 
   return (
     <div>
       <h1>This is a dynamic route</h1>
-      <Link href={"https://www.youtube.com/"}>Youtube</Link>
+      {users.map((user: { id: string; name: string }) => (
+        <div key={user.id}>
+          <p>{user.name}</p>
+        </div>
+      ))}
     </div>
   );
 }
