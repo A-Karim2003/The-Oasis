0edb76d73cf1e481458238a8ac91c04a7763ff93
Abdoulkarim <17a.karim@gmail.com>
2026-04-01 08:21:43 +0100

    WIP: syncing work across PCs

--- app/cabins/_components/CabinsList.tsx
+++ app/cabins/_components/CabinsList.tsx
@@ -1,16 +1,27 @@
 import { supabaseAdmin } from "@/lib/supabase/admin";
 import Cabin from "./Cabin";
 
+type Cabin = {
+  id: number;
+  created_at: string;
+  name: string;
+  capacity: number;
+  price: number;
+  discount: number;
+  description: string;
+  image_url: string;
+};
+
 export default async function CabinsList() {
-  const { data: cabins, error } = await supabaseAdmin
-    .from("cabins")
-    .select("*");
+  const { data } = await supabaseAdmin.from("cabins").select("*");
+
+  const cabins = data as Cabin[] | null;
+
   return (
     <div className="grid lg:grid-cols-2 gap-12 mt-5">
-      <Cabin />
-      <Cabin />
-      <Cabin />
-      <Cabin />
+      {cabins?.map((cabin) => (
+        <Cabin key={cabin.id} cabin={cabin} />
+      ))}
     </div>
   );
 }
