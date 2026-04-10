9e6f3f01899ae080ca8ebb0ca312a9373a2d08cd
Abdoulkarim <17a.karim@gmail.com>
2026-04-10 19:14:02 +0100

    fea: connect implement auth setup and connect it to supabase

--- app/api/route.ts
+++
@@ -1,7 +0,0 @@
-import type { NextRequest } from "next/server";
-
-export async function GET(request: unknown) {
-  const url = request.nextUrl;
-
-  return Response.json({ message: "success" });
-}
