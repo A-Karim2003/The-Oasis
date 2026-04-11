08a62de3edc939918e306edd8186101e71005e2b
Abdoulkarim <17a.karim@gmail.com>
2026-04-11 23:50:54 +0100

    feat: Add Google social authentication provider

--- lib/auth.ts
+++ lib/auth.ts
@@ -8,4 +8,13 @@ export const auth = betterAuth({
       rejectUnauthorized: false,
     },
   }),
+
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      accessType: "offline",
+      prompt: "select_account consent",
+    },
+  },
 });
